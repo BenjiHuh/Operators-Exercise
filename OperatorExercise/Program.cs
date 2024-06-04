91530de744cddc8878856885f167a20556b3786a
Benjamin Huh <benjaminayhuh@gmail.com>
2024-06-04 10:08:31 -0700

    attempted all excerise tasks

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            (double areaTestCase, double radiusTestCase) = AreaOfCircle();
+            Console.WriteLine($"The area of the circle with the TestCase radius is {areaTestCase} with given radius {radiusTestCase}");
+               
+        }
+
+        private static (double, double) AreaOfCircle() 
+        {
+            Console.WriteLine("Please enter a number as a radius (double or integer value)");
+            double radius = double.Parse(Console.ReadLine());
+            return (Math.PI * Math.Pow(radius, 2), radius);
         }
     }
 }
